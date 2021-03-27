1c6386f68f1cf20d69a90645845907b55baa82d6
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-03-27 16:30:33 +0530

    dynamic list added

--- routes/list/list.js
+++ routes/list/list.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const { data } = require("../../data");
+const obj = data[0];
+
+console.log(data);
+console.log(obj.Medicine_Name);
+
 router.get("/", (req, res) => {
-  res.render("pages/list");
+  res.render("pages/list", { data });
 });
 
 module.exports = router;
