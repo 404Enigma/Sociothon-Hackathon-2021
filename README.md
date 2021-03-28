578ffc30f56cd6d75ccda4e4506810182818d991
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-03-28 11:40:57 +0530

    done

--- README.md
+++
@@ -1,1 +0,0 @@
-# Sociothon-Hackathon-2021
--- utility/utility.js
+++
@@ -1,5 +0,0 @@
-const monthDiff = (dateFrom, dateTo) => {
-  return dateTo.getMonth() - dateFrom.getMonth() + 12 * (dateTo.getFullYear() - dateFrom.getFullYear());
-};
-
-module.exports = { monthDiff };
